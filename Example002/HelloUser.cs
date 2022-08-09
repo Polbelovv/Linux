821817c0cee945c653c42fc39871860e581cd711
Polbelovv <belovv1982@mail.ru>
2022-08-09 22:11:09 +0400

    add new repo

---
+++ Example002/HelloUser.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Vvedite vashe imy");
+        string username = Console.ReadLine();
+        Console.WriteLine ("Privet, ");
+        Console.WriteLine (username); 
+    }
+}
